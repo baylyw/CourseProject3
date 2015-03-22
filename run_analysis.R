bd84d0e5c3b09588a519667b3cdd6a27b38e710e
baylyw <baylyw@gmail.com>
2015-03-22 12:16:08 -0700

    Fix Data File Locations

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,4 @@
-# The purpose of this project is to demonstrate your ability to collect, work with, and clean a data 
-# set. The goal is to prepare tidy data that can be used for later analysis. You will be graded by 
-# your peers on a series of yes/no questions related to the project. You will be required to submit: 
-# 1) a tidy data set as described below, 2) a link to a Github repository with your script for 
-# performing the analysis, and 3) a code book that describes the variables, the data, and any 
-# transformations or work that you performed to clean up the data called CodeBook.md. You should also 
-# include a README.md in the repo with your scripts. This repo explains how all of the scripts work 
-# and how they are connected. 
-
-# You should create one R script called run_analysis.R that does the following. 
+# An R script called run_analysis.R that does the following. 
 # Merges the training and the test sets to create one data set.
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Uses descriptive activity names to name the activities in the data set
@@ -15,17 +6,17 @@
 # From the data set in step 4, creates a second, independent tidy data set with the average of each
 # variable for each activity and each subject.
 
-setwd("~/Desktop/Rcourse/Course3/Project")
-x.test <- read.table(file = "UCI HAR Dataset/test/X_test.txt")
-y.test <- read.table(file = "UCI HAR Dataset/test/Y_test.txt")
-subject.test <- read.table(file = "UCI HAR Dataset/test/subject_test.txt")
+# setwd("~/Desktop/Rcourse/Course3/Project/UCI HAR Dataset/")
+x.test <- read.table(file = "test/X_test.txt")
+y.test <- read.table(file = "test/Y_test.txt")
+subject.test <- read.table(file = "test/subject_test.txt")
 
-x.train <- read.table(file = "UCI HAR Dataset/train//X_train.txt")
-y.train <- read.table(file = "UCI HAR Dataset/train//y_train.txt")
-subject.train <- read.table(file = "UCI HAR Dataset/train//subject_train.txt")
+x.train <- read.table(file = "train//X_train.txt")
+y.train <- read.table(file = "train//y_train.txt")
+subject.train <- read.table(file = "train//subject_train.txt")
 
-features <- read.table(file = "UCI HAR Dataset//features.txt")
-activities <- read.table(file = "UCI HAR Dataset//activity_labels.txt")
+features <- read.table(file = "features.txt")
+activities <- read.table(file = "activity_labels.txt")
 
 colnames(y.test) <- "act.code"
 colnames(y.train) <- "act.code"
